63822e2580e2244ded23d069d25120da835b6e3f
Dimitri Dewaele <dimitri.dewaele@gmail.com>
2016-02-07 20:10:03 +0100

    use a facory

--- HelloWorldService/src/be/softwarelab/osgi/service/impl/HelloServiceActivator.java
+++ HelloWorldService/src/be/softwarelab/osgi/service/impl/HelloServiceActivator.java
@@ -5,26 +5,31 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 import be.softwarelab.osgi.service.HelloService;
+import be.softwarelab.osgi.service.HelloServiceFactory;
 
 public class HelloServiceActivator implements BundleActivator {
-	
+
 	ServiceRegistration helloServiceRegistration;
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.
+	 * BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		HelloService helloService = new HelloServiceImpl();
-        helloServiceRegistration = context.registerService(HelloService.class.getName(), helloService, null);
+		HelloServiceFactory helloServiceFactory = new HelloServiceFactory();
+		helloServiceRegistration = context.registerService(HelloService.class.getName(), helloServiceFactory, null);
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		 helloServiceRegistration.unregister();
+		helloServiceRegistration.unregister();
 	}
 
 }
