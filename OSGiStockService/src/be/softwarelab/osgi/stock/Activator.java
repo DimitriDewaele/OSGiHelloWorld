7fa7365c984275b3e6439ebc37a391cc2bd14425
Laptop User <ddewaele@cisco.com>
2016-10-05 16:57:44 +0200

    wip

--- OSGiStockService/src/be/softwarelab/osgi/stock/Activator.java
+++ OSGiStockService/src/be/softwarelab/osgi/stock/Activator.java
@@ -2,22 +2,23 @@ package be.softwarelab.osgi.stock;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import be.softwarelab.osgi.stock.impl.StockServiceFactory;
+import be.softwarelab.osgi.stock.service.StockService;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
-	}
+	ServiceRegistration stockServiceRegistration;
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext bundleContext) throws Exception {
+	public void start(BundleContext context) throws Exception {
 		System.out.println("StockService: START");
-		Activator.context = bundleContext;
+		StockServiceFactory stockServiceFactory = new StockServiceFactory();
+		stockServiceRegistration = context.registerService(StockService.class.getName(), stockServiceFactory, null);
 	}
 
 	/*
@@ -26,7 +27,7 @@ public class Activator implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		System.out.println("StockService: STOP");
-		Activator.context = null;
+		stockServiceRegistration.unregister();
 	}
 
 }
